7918dd26a08d703b5320c72a0f53eae8723d3348
Lenar Sharipov <lenarsharipov@gmail.com>
2024-05-26 20:13:46 +0300

    Logger added

---
+++ src/main/java/com/lenarsharipov/simplebank/aspect/LoggerAspect.java
@@ -0,0 +1,28 @@
+package com.lenarsharipov.simplebank.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggerAspect {
+
+    @Around("execution(* com.lenarsharipov.simplebank..*.*(..))")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("{} method execution start", joinPoint.getSignature().toString());
+        Object returnObj = joinPoint.proceed();
+        log.info("{} method execution end", joinPoint.getSignature().toString());
+        return returnObj;
+    }
+
+    @AfterThrowing(value = "execution(* com.lenarsharipov.simplebank..*(..))",throwing = "e")
+    public void logException(JoinPoint joinPoint, Exception e) {
+        log.warn("{} An exception happened due to : {}", joinPoint.getSignature(), e.getMessage());
+    }
+}
