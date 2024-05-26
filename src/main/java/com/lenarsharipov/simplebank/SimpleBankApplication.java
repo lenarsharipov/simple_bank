da7cec9b00ce77e7adc7620cdf16fc4ea6fd919c
Lenar Sharipov <lenarsharipov@gmail.com>
2024-05-26 20:15:29 +0300

    app entry point added

---
+++ src/main/java/com/lenarsharipov/simplebank/SimpleBankApplication.java
@@ -0,0 +1,16 @@
+package com.lenarsharipov.simplebank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+@EnableScheduling
+public class SimpleBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBankApplication.class, args);
+    }
+}
