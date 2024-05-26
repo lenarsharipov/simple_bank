3282391b14a07abb4854b5d110f8b0eec945f17a
Lenar Sharipov <lenarsharipov@gmail.com>
2024-05-26 20:17:02 +0300

    exception added

---
+++ src/main/java/com/lenarsharipov/simplebank/exception/UniqueFieldException.java
@@ -0,0 +1,8 @@
+package com.lenarsharipov.simplebank.exception;
+
+public class UniqueFieldException extends RuntimeException {
+
+    public UniqueFieldException(String message) {
+        super(message);
+    }
+}
