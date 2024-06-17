7fbd22c8555db7b958face8fa76293eb86442ffc
Lenar Sharipov <lenarsharipov@gmail.com>
2024-06-17 15:31:51 +0300

    Redis dependency added.Enabled Caching

--- src/main/java/com/lenarsharipov/simplebank/SimpleBankApplication.java
+++ src/main/java/com/lenarsharipov/simplebank/SimpleBankApplication.java
@@ -2,6 +2,7 @@ package com.lenarsharipov.simplebank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableTransactionManagement
 @EnableScheduling
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableCaching
 public class SimpleBankApplication {
 
     public static void main(String[] args) {
